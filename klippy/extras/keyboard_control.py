2f01fef74db4943d4ac75d34216b8d9f87234cb0
GermanTortoise <yunhanluo06@gmail.com>
2026-02-24 15:08:50 -0500

    Complete startup Gcode

--- klippy/extras/keyboard_control.py
+++ klippy/extras/keyboard_control.py
@@ -38,15 +38,13 @@ M204 S5000 T5000
 G28 ; home all
 ;M190 S50 ; wait for bed temp
 G1 Z1.24
-G1 X100 Y100 ; TODO: don't hardcode this
-G1 Z.24
-; TODO: finish
 """
 
 class G1(NamedTuple):
-    x: float
-    y: float
-    e: float
+    x: float | None
+    y: float | None
+    z: float | None
+    e: float | None
 
 class KeyboardControl:
     def __init__(self, config):
@@ -66,8 +64,8 @@ class KeyboardControl:
         self.x = (self.x_max - self.x_min) / 2
         self.y = (self.y_max - self.y_min) / 2
         self.z = self.layer_height
-        self.gcode.register_command('ETCH_START', self.run, desc='start etching, MOCK=1 for mock input')
-        self.gcode.register_command('ETCH_STOP', self.stop, desc='stop etching, can also stop by closing the window')
+        self.gcode.register_command('ETCH_START', self.cmd_ETCH_START, desc='start etching, MOCK=1 for mock input')
+        self.gcode.register_command('ETCH_STOP', self.cmd_ETCH_STOP, desc='stop etching, can also stop by closing the window')
         
     def _increment_bounded(self, val: float, move: float, min: float, max: float) -> float:
         if val + move < min:
@@ -76,8 +74,17 @@ class KeyboardControl:
             return max
         return val + move
     
-    def _move_gcode(self, move: G1) -> str:
-        return f"G1 X{move.x:.3f} Y{move.y:.3f} E{move.e:.5f}"
+    def _G1_gcode(self, move: G1) -> str:
+        out = "G1"
+        if move.x:
+            out += f" X{move.x:.3f}"
+        if move.y:
+            out += f" Y{move.y:.3f}"
+        if move.z:
+            out += f" Z{move.z:.3f}"
+        if move.e:
+            out += f" E{move.e:.5f}"
+        return out
     
     def _distance(self, x1: float, y1: float, x2: float, y2: float) -> float:
         return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
@@ -100,26 +107,30 @@ class KeyboardControl:
         self.y = self._increment_bounded(self.y, v.y, self.y_min, self.y_max)
         # v.rho = move distance
         # slic3r extrusion formula (___) shaped
-        e_length = v.rho * (math.pi * (self.layer_height / 2) ** 6 + (self.line_width - self.layer_height) * self.layer_height)
-        return G1(self.x, self.y, e_length)
+        e_length = v.rho * (math.pi * (self.layer_height / 2) ** 2 + (self.line_width - self.layer_height) * self.layer_height)
+        return G1(self.x, self.y, None, e_length)
         
-    def run(self, gcmd):
-        gcmd.respond_info("Started etching!")
+    def cmd_ETCH_START(self, gcmd):
         mock = gcmd.get_int('MOCK', 0)
         if mock > 0:
             mock_input = True
             gcmd.respond_info("Started etching with mock input")
         else:
             mock_input = False
+            gcmd.respond_info("Started etching!")
         
         pygame.init()
         self.clock = pygame.time.Clock()
         self.running = True
         self.screen = pygame.display.set_mode((100, 100))
         pygame.display.set_caption("Keyboard Control")
-        gcmd.respond_info("Finished etch init")
+        gcmd.respond_info("Finished pygame init")
         
         self.gcode.run_script_from_command(startup_command)
+        center = self._G1_gcode(G1(self.x, self.y, None, None))
+        down = self._G1_gcode(G1(None, None, self.z, None))
+        self.gcode.run_script_from_command(center)
+        self.gcode.run_script_from_command(down)
         
         frame_count = 0
         # TODO: refactor this less stupidly
@@ -151,17 +162,16 @@ class KeyboardControl:
             
             move = self._move(keys_pressed)
             if move.e > 0:
-                gcode_move = self._move_gcode(move)
+                gcode_move = self._G1_gcode(move)
                 # print(gcode_move)
                 self.gcode.run_script_from_command(gcode_move)
             
             frame_count += 1
         
         pygame.quit()
-        
         gcmd.respond_info("Finished etching")
         
-    def stop(self, gcmd):
+    def cmd_ETCH_STOP(self, gcmd):
         # don't think this can work cuz [start] is running and can't be interuppted
         gcmd.respond_info("Stop etching")
         if self.running:
@@ -207,5 +217,5 @@ class MockGcode:
     
 if __name__ == "__main__":
     e = load_config(MockConfig()) 
-    e.run()
+    e.cmd_ETCH_START()
     
